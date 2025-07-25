732b9f3281f25f242992d240eab00194106e22f9
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-07-25 20:27:51 +0100

    init

---
+++ src/components/MovieGrid.tsx
@@ -0,0 +1,29 @@
+import css from './MovieGrid.module.css';
+import { Movie } from '../types/movie';
+
+interface MovieGridProps {
+    movies: Movie[];
+    onSelect: (movie: Movie) => void;
+}
+
+const MovieGrid = ({ movies, onSelect }: MovieGridProps) => {
+    return (
+        <ul className={css.grid}>
+            {movies.map((movie) => (
+                <li key={movie.id}>
+                <div className={css.card} onClick={() => onSelect(movie)}>
+                  <img 
+                        className={css.image} 
+                        src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" 
+                        alt={movie.title}
+                        loading="lazy" 
+                      />
+                    <h2 className={css.title}>Movie title</h2>
+                </div>
+              </li>
+            ))}
+        </ul>
+    )
+}
+
+export default MovieGrid;
---
+++ src/services/movieService.ts
@@ -0,0 +1,20 @@
+import axios, { type AxiosResponse } from "axios";
+import { Movie } from "../types/movie";
+
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const myKey = import.meta.env.VITE_TMDB_TOKEN;
+
+interface FetchMoviesResponse {
+    results: Movie[];
+}
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+    const config = {
+        params: { query },
+        headers: {
+            Autorisation: `Bearer ${myKey}`
+        }
+    }
+    const response: AxiosResponse<FetchMoviesResponse> = await axios.get(BASE_URL, config);
+    return response.data.results;
+}
